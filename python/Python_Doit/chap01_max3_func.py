9fce0e4fdf9ae628212078018e09fb67e1ce8507
jimmy <wandering_jin@naver.com>
2024-02-14 02:42:42 +0900

    1

--- python/Python_Doit/chap01_max3_func.py
+++ python/Python_Doit/chap01_max3_func.py
@@ -1,5 +1,19 @@
 # 세 정수의 최댓값 구하기
 
-def max(a, b, c):
+import random
+
+
+def max3(a, b, c):
     maximum = a
-    if(b >)
+    if b > maximum:
+        maximum = b
+    if c > maximum:
+        maximum = c
+    return maximum
+
+
+for i in range(10):
+    a = random.randint(0,9)
+    b = random.randint(0,9)
+    c = random.randint(0,9)
+    print(f'math3({a}, {b}, {c}) = {max3({a},{b},{c})}')
