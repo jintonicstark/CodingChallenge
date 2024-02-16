eca1785e6f06f5a0d9a803e3b75f98113ac1c1b8
jintonicstark <wandering_jin@naver.com>
2024-02-17 00:30:04 +0900

    tpt

---
+++ python/Python_Doit/chap02_total.py
@@ -0,0 +1,20 @@
+# 학생 5명의 시험 점수를 입력받아 합계와 평균을 출력하기
+
+print('학생들의 점수를 입력학세요')
+
+score1 = int(input('1번학생의 점수를 입력하세요'))
+score2 = int(input('2번학생의 점수를 입력하세요'))
+score3 = int(input('3번학생의 점수를 입력하세요'))
+score4 = int(input('4번학생의 점수를 입력하세요'))
+score5 = int(input('5번학생의 점수를 입력하세요'))
+
+total = 0;
+total += score1
+total += score2w
+total += score3
+total += score4
+total += score5
+
+print(f'합계는 {total} 입니다.')
+print(f'평균은 {total/5} 입니다.')
+
